3948ce2dcef07b1d5a15ce78bd5652c8e9a22c0a
pranali2808 <ranepranali1997@gmail.com>
2022-04-16 12:31:05 +0530

    user need to enter valid first name

---
+++ Name.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Name
+    {
+        public void First()
+        {
+            string firstName;
+            var regex = "^[A-Z][a-z]{2,}$";
+            Console.WriteLine("Enter First Name: ");
+            firstName = Console.ReadLine();
+            var match = Regex.Match(firstName, regex);
+            if (match.Success)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+        }
+    }
+}
+    
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User registration problem");
+            Name first = new Name();
+            first.First();
+
+            Console.ReadLine();
+        }
+       
+    }
+}
